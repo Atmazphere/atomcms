4b183b06728906c38520b2adcf762a38d8517590
Object <87041394+ObjectRetros@users.noreply.github.com>
2023-02-06 21:11:48 +0100

    ⬆️ Upgrade Laravel 9.x to Laravel 10.x (#140)
    * Apply Laravel coding style
    
    Shift automatically applies the Laravel coding style - which uses the PSR-12 coding style as a base with some minor additions.
    
    You may customize the code style applied by configuring [Pint](https://laravel.com/docs/pint), [PHP CS Fixer](https://github.com/FriendsOfPHP/PHP-CS-Fixer), or [PHP CodeSniffer](https://github.com/squizlabs/PHP_CodeSniffer) for your project root.
    
    For more information on customizing the code style applied by Shift, [watch this short video](https://laravelshift.com/videos/shift-code-style).
    
    * Convert `optional()` to nullsafe operator
    
    * Shift core files
    
    * Shift config files
    
    * Bump Laravel dependencies
    
    * Adopt anonymous migrations
    
    * Update method signatures for Laravel contracts
    
    * Add type hints for Laravel 10
    
    * Add type hints from DocBlocks
    
    * Remove redundant typing in DocBlocks
    
    * Shift cleanup
    
    * Update return types to match laravel 10 properly
    
    * Fix update password and 2FA verification issues
    
    * Update docker file
    
    * Format files
    
    ---------
    
    Co-authored-by: Shift <shift@laravelshift.com>

--- resources/themes/atom/views/components/footer.blade.php
+++ resources/themes/atom/views/components/footer.blade.php
@@ -1,5 +1,6 @@
 <footer
-    class="w-full h-14 bg-gray-100 dark:bg-gray-900 mt-auto flex flex-col justify-center md:flex-row md:justify-center text-gray-400 items-center md:px-8 text-sm" onclick="showFooter()">
+    class="mt-auto flex h-14 w-full flex-col items-center justify-center bg-gray-100 text-sm text-gray-400 dark:bg-gray-900 md:flex-row md:justify-center md:px-8"
+    onclick="showFooter()">
     <div class="md:font-semibold text-[12px] md:text-[14px] cursor-pointer hover:underline">
         &copy {{ date('Y') }} -
         {{ __(':hotel is a not for profit educational project', ['hotel' => setting('hotel_name')]) }}
@@ -15,18 +16,34 @@
 
 <script>
     function showFooter() {
-        const creator = '<a class="text-blue-400 underline" href="https://devbest.com/threads/atom-cms-a-multi-theme-cms.93034/" target="_blank">Object</a>';
-        const creator2 = '<a class="text-blue-400 underline" href="https://habplus.org/profile/Tyler" target="_blank">Tyler</a>';
+        const creator =
+            '<a class="text-blue-400 underline" href="https://devbest.com/threads/atom-cms-a-multi-theme-cms.93034/" target="_blank">Object</a>';
         const credits = [
-            '<strong>All Contributors</strong> Helping bring Atom to life<br/>',
+            '<strong>Kasja</strong> Helping with design, ideas & GFX <br/>',
+            '<strong>Nicollas </strong> Dark mode, Turbolinks, Performance improvements, Article reactions, User sessions, Layout improvements & PT-BR translations <br/>',
+            '<strong>Kani</strong> RCON System & Findretros API <br/>',
+            '<strong>Dominic</strong> Performance improvements & User sessions <br/>',
+            '<strong>Oliver</strong> Profile page & Finnish translations <br/>',
+            '<strong>Beny</strong> Findretros API Fixes & CF Fixes <br/>',
+            '<strong>Live</strong> French translations, bugfixes & tweaks <br/>',
+            '<strong>DamienJolly</strong> Bugfixes <br/>',
+            '<strong>Danbo</strong> Bugfixes <br/>',
+            '<strong>Diddy/Josh</strong> Code readability improvements <br/>',
+            '<strong>Damue</strong> German translations <br/>',
+            '<strong>Talion</strong> Turkish translations <br/>',
+            '<strong>CentralCee & Rille</strong> Swedish translations <br/>',
+            '<strong>Yannick</strong> Netherlands translations <br/>',
+            '<strong>Gedomi</strong> Spanish translations <br/>',
+            '<strong>Lorenzune</strong> Italian translations <br/>',
+            '<strong>Twana</strong> Norwegian translations <br/>'
         ];
-        const content = '{{ __('Thank you for playing :hotel. Our team has put a lot of effort into making the hotel what it is. We truly appreciate you for being here to reminisce the old times❤️', ['hotel' => setting('hotel_name')]) }}';
+        const content =
+            '{{ __('Thank you for playing :hotel. We have put a lot of effort into making the hotel what it is, and we truly appreciate you being here❤️', ['hotel' => setting('hotel_name')]) }}';
         const drivenBy = '{{ __(':hotel is driven by Atom CMS made by:', ['hotel' => setting('hotel_name')]) }}';
-        const drivenBy2 = '{{ __('With further love from:', ['hotel' => setting('hotel_name')]) }}';
 
         Swal.fire(
             '<span class="text-[26px]">{{ setting('hotel_name') }}</span>',
-            `<span class="text-sm">${content}<br/><br/>${drivenBy} ${creator}<br/><br>${drivenBy2} ${creator2}<br/><span class="flex flex-col space-y-2"></span></span>`,
+            `<span class="text-sm">${content}<br/><br/>${drivenBy} ${creator}<br/><br/><span class="flex flex-col space-y-2">Credits:<br/>${credits.join('')}</span></span>`,
             'question'
         )
     }
